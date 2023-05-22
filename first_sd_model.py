d837e7e905058671feb28198a8bf9e2aa9b79d1a
曾永源 <zaidentseng@Zaidens-MacBook-Air.local>
2023-05-22 21:23:27 +0800

    Zaiden1-create_my_sd

---
+++ first_sd_model.py
@@ -0,0 +1,15 @@
+from diffusers import DiffusionPipeline
+
+pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+pipe = pipe.to("mps")
+
+# Recommended if your computer has < 64 GB of RAM
+pipe.enable_attention_slicing()
+
+prompt = "a photo of an astronaut riding a horse on mars"
+
+# First-time "warmup" pass if PyTorch version is 1.13 (see explanation above)
+_ = pipe(prompt, num_inference_steps=1)
+
+# Results match those from the CPU device after the warmup pass.
+image = pipe(prompt).images[0]
